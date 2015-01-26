54393e97ea73ac0fdeb19b9074cfb1f6ed13ff68
mjmeli <mjmeli@ncsu.edu>
2015-01-26 02:11:59 -0500

    CSV file support

--- M4A1MarketTrendTrack.py
+++ M4A1MarketTrendTrack.py
@@ -1,10 +1,11 @@
-# Every 5 minutes, checks to see whether the price is high or low and record it in a JSON file
+# Every 5 minutes, checks to see whether the price is high or low and record the occurence in a JSON file and record the time in a CSV file
 
-import urllib.request, json, time, datetime
+import urllib.request, json, time, datetime, csv
 from time import sleep
 
 url = "http://steamcommunity.com/market/priceoverview/?country=US&currency=1&appid=730&market_hash_name=M4A1-S%20%7C%20Guardian%20%28Minimal%20Wear%29"
 jsonFileName = "data.json"
+csvFileName = "data.csv"
 lowPrice = 4.70
 highPrice = 5.45
 checked = False
@@ -39,11 +40,16 @@ while True:
 			if lowestPrice >= highPrice:
 				dataJSON['High'][str(time.hour)][str(time.minute)] = dataJSON['High'][str(time.hour)][str(time.minute)] + 1
 				print ("High price recorded")
+
+		# Write data to CSV file
+		with open(csvFileName, "a+") as f:
+			csvFile = csv.writer(f)
+			csvFile.writerow([str(time.hour) + ":" + str(time.minute), str(lowestPrice)])
 		
-		# Write back and close
-		jsonFile = open(jsonFileName, "w")
-		json.dump(dataJSON, jsonFile)
-		jsonFile.close()
+		# Write back JSON and close
+		with open(jsonFileName, "w") as jsonFile:
+			json.dump(dataJSON, jsonFile)
+
 	else:
 		# Reset the checked flag if it is not a 5 minute increment
 		if checked and time.minute % 5 != 0:
